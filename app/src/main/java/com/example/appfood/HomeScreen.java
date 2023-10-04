45aa03d9a235153fb3e106d6bf7b1b964ed4f7ba
exz121 <szymentu121@gmail.com>
2023-10-04 12:53:34 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/R3QAVAILABLE/AppFOOD""
    This reverts commit 2b022082faacf7388e4f78a4a945b6d0957f7033.

--- app/src/main/java/com/example/appfood/HomeScreen.java
+++ app/src/main/java/com/example/appfood/HomeScreen.java
@@ -1,14 +1,37 @@
 package com.example.appfood;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
     public class HomeScreen extends AppCompatActivity {
+        @SuppressLint("MissingInflatedId")
         @Override
         protected void onCreate(Bundle savedInstanceState) {
+
             super.onCreate(savedInstanceState);
             setContentView(R.layout.homescreenlayout);
+            Button buttonNavigate;
+
+
+            buttonNavigate = findViewById(R.id.buttonprofile);
+
+            // Obsługa kliknięcia w przycisk
+            buttonNavigate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Tworzenie intencji do przeniesienia użytkownika do innej aktywności
+                    Intent intent = new Intent(HomeScreen.this, Profile.class);
+
+                    // Rozpoczęcie nowej aktywności
+                    startActivity(intent);
+                }
+            });
+
 
         }
     }
