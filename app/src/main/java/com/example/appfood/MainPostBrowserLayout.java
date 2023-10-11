62723c1ac68797519e705a8578fe05a18bba1f6b
D1ablo12 <diablo4899@gmail.com>
2023-10-11 10:27:55 +0200

    new home screen

--- app/src/main/java/com/example/appfood/MainPostBrowserLayout.java
+++ app/src/main/java/com/example/appfood/MainPostBrowserLayout.java
@@ -2,13 +2,29 @@ package com.example.appfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainPostBrowserLayout extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_post_browser_layout);
+        ImageView profile;
+        profile=findViewById(R.id.goto_profile);
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainPostBrowserLayout.this, Profile.class);
+                startActivity(intent);
+            }
+        });
     }
 }
