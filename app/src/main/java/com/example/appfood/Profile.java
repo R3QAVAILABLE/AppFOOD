2547949b68ca2b4ba733a3550db88d3223e2ee35
R3QAVAILABLE <marek.koszuta1337@gmail.com>
2023-10-25 00:27:27 +0200

    profil update

--- app/src/main/java/com/example/appfood/Profile.java
+++ app/src/main/java/com/example/appfood/Profile.java
@@ -1,96 +1,190 @@
 package com.example.appfood;
 
-import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-public class Profile extends AppCompatActivity {
-
-    private TextView textViewName, textViewEmail;
+public class Profile extends Activity {
+    private ImageView imageViewAvatar;
+    private TextView textViewEmail;
+    private TextView textViewName;
+    private TextView textViewUsername;
+    private EditText editTextNewUsername;
+    private Button buttonSaveUsername;
+    private Button buttonUploadImage;
+    private ImageView imageViewUploaded;
+    private Uri imageUri;
+    private FirebaseStorage storage;
+    private FirebaseAuth auth;
     private FirebaseFirestore firestore;
+    private static final int PICK_IMAGE_REQUEST = 1;
 
-    FirebaseAuth auth;
-    Button button;
-
-    TextView textView;
-
-    FirebaseUser user;
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
-        auth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.logout);
-        textView = findViewById(R.id.user_details);
-        user = auth.getCurrentUser();
-
-        // Inicjalizacja widoków
-        textViewName = findViewById(R.id.textViewName);
         textViewEmail = findViewById(R.id.textViewEmail);
+        textViewName = findViewById(R.id.textViewName);
+        textViewUsername = findViewById(R.id.textViewNazwa);
+        editTextNewUsername = findViewById(R.id.editTextNewUsername);
+        buttonSaveUsername = findViewById(R.id.buttonSaveUsername);
+        imageViewUploaded = findViewById(R.id.imageViewUploaded);
+        buttonUploadImage = findViewById(R.id.buttonUploadImage);
 
-        // Inicjalizacja Firestore
+        storage = FirebaseStorage.getInstance();
+        auth = FirebaseAuth.getInstance();
         firestore = FirebaseFirestore.getInstance();
 
-        // Pobieranie danych z Firestore
-        firestore.collection("users")
-                .document("your_user_document_id") // Zastąp 'your_user_document_id' odpowiednim ID użytkownika
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            DocumentSnapshot document = task.getResult();
-                            if (document.exists()) {
-                                String name = document.getString("name");
-                                String email = document.getString("email");
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+            firestore.collection("users").document(userId).get()
+                    .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            if (documentSnapshot.exists()) {
+                                String email = documentSnapshot.getString("email");
+                                String name = documentSnapshot.getString("name");
+                                String username = documentSnapshot.getString("username");
 
-                                // Wyświetlenie informacji o użytkowniku w widoku
-                                textViewName.setText("Imię: " + name);
                                 textViewEmail.setText("E-mail: " + email);
-                            } else {
-                                Log.d("UserProfileActivity", "Brak dokumentu");
+                                textViewName.setText("Name: " + name);
+                                textViewUsername.setText("Username: " + username);
                             }
-                        } else {
-                            Log.d("UserProfileActivity", "Błąd pobierania dokumentu", task.getException());
                         }
-                    }
-                });
-
-        if(user==null){
-            Intent intent = new Intent(getApplicationContext(),LogIn.class);
-            startActivity(intent);
-            finish();
+                    });
         }
-        else{
-            textView.setText(user.getEmail());
 
-        }
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonSaveUsername.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(),LogIn.class);
-                startActivity(intent);
-                finish();
+                String newUsername = editTextNewUsername.getText().toString();
+                if (!newUsername.isEmpty()) {
+                    textViewUsername.setText("Username: " + newUsername);
+                    updateUsernameInFirestore(newUsername);
+                }
             }
         });
 
+        buttonUploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImagePicker();
+            }
+        });
+
+        // Ładowanie obrazka z Firebase Storage
+        loadProfileImage();
+    }
+
+    private void openImagePicker() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageUri = data.getData();
+            imageViewUploaded.setImageURI(imageUri);
+            imageViewUploaded.setVisibility(View.VISIBLE);
+
+            // Zapisywanie obrazka do Firebase Storage
+            uploadImageToFirebaseStorage();
+        }
+    }
+
+    private void updateUsernameInFirestore(String newUsername) {
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+
+            firestore.collection("users").document(userId)
+                    .update("username", newUsername)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                // Zaktualizowano nazwę użytkownika pomyślnie
+                                Toast.makeText(Profile.this, "Username updated successfully", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // Obsłuż błąd
+                                Toast.makeText(Profile.this, "Failed to update username", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void uploadImageToFirebaseStorage() {
+        if (imageUri != null) {
+            StorageReference storageRef = storage.getReference().child("profile_images/" + auth.getCurrentUser().getUid() + ".jpg");
+
+            storageRef.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            // Przesłanie obrazka pomyślnie
+                            Toast.makeText(Profile.this, "Image uploaded successfully", Toast.LENGTH_SHORT).show();
+
+                            // Teraz można pobrać URL obrazka i zapisać go w Firestore
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(Exception e) {
+                            // Obsłuż błąd
+                            Toast.makeText(Profile.this, "Failed to upload image", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    private void loadProfileImage() {
+        StorageReference storageRef = storage.getReference().child("profile_images/" + auth.getCurrentUser().getUid() + ".jpg");
+
+        storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                // Pobieranie URL obrazka z Firebase Storage
+                String imageUrl = uri.toString();
+
+                // Wyświetlenie obrazka w ImageView
+                imageViewUploaded.setImageURI(Uri.parse(imageUrl));
+                imageViewUploaded.setVisibility(View.VISIBLE);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+                // Obsłuż błąd, jeśli obrazka nie udało się pobrać
+                Toast.makeText(Profile.this, "Failed to load profile image", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
